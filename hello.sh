23284d09df4b314ba4963e23e00facbbd0e566c7
sli <shcli@ucsc.edu>
2019-09-03 11:20:53 -0700

    stash apply and merge conflict

--- hello.sh
+++ hello.sh
@@ -5,17 +5,18 @@ function sayHi {
   local HELLO=hi
   echo $HELLO
 }
-echo "real hello"
+echo "real hello "
 echo $HELLO
 
-echo "fake hello"
+echo "fake hello "
 sayHi
 
-echo "printing top system info `top`"
+#echo "printing top system info `top`"
 
+#echo "what's your name?"
+#read name
+#echo "helloooooo $name"
 
-echo "what's your name?"
-read name
-echo "helloooooo $name"
-
-echo "printing uname info `uname -a`"
+echo "printing uname info\n`uname -a`"
+echo '\n'
+echo `ls -al | egrep '.sh'`
